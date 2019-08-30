bb941b4b02d66e20f0a9abde420ecd4700144a57
Coleman Shepard <colemanshepard@gmail.com>
2019-08-30 18:54:53 -0400

    modified:   images/front.png 	new file:   images/money.png 	modified:   index.css 	modified:   index.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const ov = document.querySelector("#over");
 
 
 ov.addEventListener("mousemove", (e) => {
-  el.style.backgroundPositionX = -e.offsetX + "px";
-  el.style.backgroundPositionY = -e.offsetY + "px";
+  el.style.backgroundPositionX = -e.offsetX/4 + "px";
+  el.style.backgroundPositionY = -e.offsetY/4 + "px";
 });
